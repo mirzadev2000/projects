68d51365bd789a67529c64b88336a662f28bb9cf
mirzadev2000 <155643804+mirzadev2000@users.noreply.github.com>
2024-01-20 20:34:40 +0100

    Add files via upload

---
+++ 28-wikipedia/final/wiki-urls.js
@@ -0,0 +1,6 @@
+const url =
+  'https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch=searchValue';
+
+
+
+const page_url = 'href=http://en.wikipedia.org/?curid=${pageid}';
